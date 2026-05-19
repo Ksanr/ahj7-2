4555bff910a14faaa0dcd01eb6ea4c743c0934fd
Aleksandr Lipunov <alipunov@mail.ru>
2026-05-19 01:23:18 +0000

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
@@ -23,46 +26,37 @@ module.exports = {
             }
           }
         },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    port: 9000,
-    proxy: [
-      {
-        context: ['/'],
-        target: 'http://localhost:7070',
-        changeOrigin: true,
-      },
-    ],
-  },
-};
-
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  return {
-    // ... остальные настройки ...
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
     plugins: [
-      // ... HtmlWebpackPlugin ...
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
       new webpack.DefinePlugin({
         'process.env.API_URL': JSON.stringify(
           isProduction
-            ? process.env.API_URL || 'https://helpdesk-backend-production.up.railway.app'  // ваш Railway URL
+            ? process.env.API_URL || 'https://helpdesk-backend-production.up.railway.app'  // замените на ваш Railway URL
             : 'http://localhost:7070'
         )
-      })
+      }),
     ],
+    devServer: {
+      port: 9000,
+      proxy: isProduction ? undefined : [
+        {
+          context: ['/'],
+          target: 'http://localhost:7070',
+          changeOrigin: true,
+        },
+      ],
+    },
   };
 };
